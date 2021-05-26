3f61d84630575e0a798c1113980c20bfab42e27e
Marceline Ball <marchettiball@gmail.com>
2021-05-26 11:46:18 -0600

    Add test for happy path user flow for form

--- cypress/integration/form_spec.js
+++ cypress/integration/form_spec.js
@@ -20,4 +20,22 @@ describe('form functionality', () => {
       .type('3')
       .should('have.value', 3)
   })
+
+  it('should be able to add a new reservation to the page', () => {
+    cy.get('input[name="name"]')
+      .type('Marceline')
+      .get('input[name="date"]')
+      .type('05/26')
+      .get('input[name="time"]')
+      .type('7:00 pm')
+      .get('input[name="guests"]')
+      .type('3')
+      .get('form > button').click()
+
+    cy.get('.resy-container')
+      .get('.name').last().contains('Marceline')
+      .get('.date').last().contains('05/26')
+      .get('.time').last().contains('7:00 pm')
+      .get('.guests').last().contains('3')
+  })
 })
