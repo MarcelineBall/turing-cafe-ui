a2725a9faf7ace102001feb3e65dcede77f5978b
Marceline Ball <marchettiball@gmail.com>
2021-05-26 09:29:44 -0600

    Add basic card structure

---
+++ src/Card/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return(
+    <article>
+      <h2>name</h2>
+      <p>date</p>
+      <p>time</p>
+      <p>number of guests</p>
+  )
+}
+
+export default Card
